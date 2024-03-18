aca6cbd76104b8abcd711dfe1a1d5d723dadc1d7
Barry Guglielmo <barry.guglielmo@schrodinger.com>
2024-03-18 16:33:58 -0400

    container works locally

--- chef/scinamic_runner.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /vol/bluebird/seurat/pro_serv/scinamic_integration
-echo "Today's date is: $(date +'%Y-%m-%d')" | tee -a test.txt
-source /vol/bluebird/seurat/pro_serv/scinamic_integration/venv/bin/activate
-echo  'source' | tee -a test.txt
-python /vol/bluebird/seurat/pro_serv/scinamic_integration/main.py -i
